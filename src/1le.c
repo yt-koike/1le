7fc0d5088148a25d25958bbea8a705cba39f6eda
Sodium11 <42059282+Sodium11@users.noreply.github.com>
2018-08-10 00:10:57 +0900

    input upgrade

--- src/1le.c
+++ src/1le.c
@@ -12,7 +12,7 @@ int linesize;
 
 int in(char* input){
 do{
-gets(input);
+fgets(input,LEN_SIZE,stdin);
 //if(strlen(input)<=0)printf("NULL string!\n");
 }while(strlen(input)<=0);
 return strlen(input);
